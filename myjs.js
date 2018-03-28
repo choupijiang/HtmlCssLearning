32496afd8f30743a562b1a552e8010ffb9921e39
张生虎 <zhangshenghu@zhangshenghudeMacBook-Pro.local>
2018-03-28 11:42:14 +0800

    javascript learning

--- myjs.js
+++ myjs.js
@@ -1,4 +1,24 @@
-var x = prompt("Where does the pope live?")
-if (x == "Vatican"){
-    alert("Correct!")
+// var x = prompt("Where does the pope live?")
+// if (x == "Vatican") {
+//     alert("Correct!")
+// }
+
+function expandLoris() {
+    var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which make up the genus Nycticebus. " +
+        "They have a round head, narrow snout, large eyes, and a variety of distinctive coloration patterns that are species-dependent. " +
+        "The hands and feet of slow lorises have several adaptations that give them a pincer-like grip and enable them to grasp branches " +
+        "for long periods of time. Slow lorises have a toxic bite, a rare trait among mammals.";
+    document.getElementById("slowLoris").innerHTML = expandedParagraph;
+}
+
+function makeInvisible() {
+    document.getElementById("fruit").className="hidden"
+}
+
+function doubleSize() {
+    var ps = document.getElementsByTagName('p')
+    for (var i = 0; i< ps.length; i++){
+        ps[i].style.fontSize = "2em";
+        ps[i].style.color="blue";
+    }
 }
